15a79dddb8d0f4763748272e0537fd559f59b05d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-09-18 09:33:41 +0200

    init math inside 10

---
+++ Solve_math_within_10/simple_math.py
@@ -0,0 +1,45 @@
+import random, time
+
+yell = '\033[33m'
+cyan = '\033[36m'
+noc = '\033[0m'
+white = '\033[0;37m'
+green = '\033[32m'
+upper_bound = 20
+
+while True:
+    a = random.randint(1, upper_bound)
+    b = random.choice([upper_bound % a, upper_bound % a - 1])
+    #b = random.randint(0, 9)
+    s = random.choice(['+', '-'])
+    #s = random.choice(['+', '-', 'x', '÷', '/'])
+    res = 1e9
+    if a < b:
+        a, b = b, a
+    if (s == '/' or s == '÷') and b == 0:
+        continue
+    time.sleep(0.9)
+    print(yell + str(a), s, b, noc)
+    guess = input(white + 'Your answer is: ' + noc)
+    if guess == 'exit' or guess == 'end':
+        break
+    if guess == 'next':
+        continue
+    if not guess.isdigit():
+        continue
+    #print()
+    if s == '+':
+        res = a + b
+    elif s == '-':
+        res = a - b
+    elif s == 'x':
+        res = a * b
+    elif s == '÷' or s == '/':
+        res = a // b
+    if res == int(guess):
+        print(f'{green} ➜ Correct! {noc}')
+    else:
+        print(f'{cyan} ➜ Wrong! {noc}')
+        print(f'{green} ➜ The answer is {res} {noc}')
+
+print(white + 'see you next time :)' + noc)
