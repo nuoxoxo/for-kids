38f8ea42a0667b675421f881c5ed01f1ebbaa56a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-09-23 23:46:57 +0200

    ud. solver

--- MasterMind/mastermind.py
+++ MasterMind/mastermind.py
@@ -1,4 +1,4 @@
-import random, time
+import random
 
 W = '\033[0;37m'
 G = '\033[32m'
@@ -6,7 +6,7 @@ Y = '\033[33m'
 C = '\033[36m'
 No = '\033[0m'
 
-def count_correct_both(code, breaker) -> int:
+def count_right_place(code, breaker) -> int:
     if len(code) != len(breaker):
         return -1
     res = 0
@@ -15,26 +15,27 @@ def count_correct_both(code, breaker) -> int:
             res += 1
     return res
 
-def count_correct_color(maker, breaker) -> int:
+def count_right_color(maker, breaker) -> int:
     if len(maker) != len(breaker):
         return -1
     code = maker.copy()
-    for i in range(len(code)):
-        if code[i] == breaker[i]:
-            code[i] = None
     freq = {}
     for n in code:
-        if not n:
-            continue
-        if n not in freq:
-            freq[n] = 1
-        else:
-            freq[n] += 1
+        if n:
+            if n not in freq:
+                freq[n] = 1
+            else:
+                freq[n] += 1
+    for i in range(len(code)):
+        if code[i] == breaker[i]:
+            freq[code[i]] -= 1
+    # print(freq) # debugger
     res = 0
-    for n in breaker:
-        if n in freq and freq[n] != 0:
-            res += 1
+    for i in range(len(breaker)):
+        n = breaker[i]
+        if n != code[i] and n in freq and freq[n] != 0:
             freq[n] -= 1
+            res += 1
     return res
 
 def printer(records) -> None:
@@ -46,30 +47,27 @@ def printer(records) -> None:
 
 # Game block
 
-while True:
-    choices = [1, 2, 3, 4, 5]#, 6, 7, 8]
-    codemaker = []
-    records = []
-    for _ in range(4):
-        codemaker.append(str(random.choice(choices)))
-    # input(codemaker)
-    for _ in range(10):
-        record = []
-        codebreaker = []
-        guess = input("input: ")
-        if guess == 'exit':
-            print("The code is: ", ''.join(codemaker))
-            exit()
-        for i in range(4):
-            codebreaker.append(guess[i])
-        correct_both = count_correct_both(codemaker, codebreaker)
-        correct_color = count_correct_color(codemaker, codebreaker)
-        record.extend( (guess, correct_both, correct_color) )
-        records.append(record)
-        if correct_both == len(codemaker):
-            print("Correct! The code is: ", ''.join(codemaker))
-            exit()
-        else:
-            printer(records)
-            # print("both right:", correct_both, "\tcolor right:", correct_color)
-        
+choices = [1, 2, 3, 4]#, 5]#, 6, 7, 8]
+codemaker = []
+records = []
+for _ in range(4):
+    codemaker.append(str(random.choice(choices)))
+for _ in range(10):
+    record = []
+    codebreaker = []
+    guess = input("input: ")
+    if guess == 'exit':
+        print("The code is: ", ''.join(codemaker))
+        exit()
+    for i in range(4):
+        codebreaker.append(guess[i])
+    right_place = count_right_place(codemaker, codebreaker)
+    right_color = count_right_color(codemaker, codebreaker)
+    record.extend( (guess, right_place, right_color) )
+    records.append(record)
+    if right_place == len(codemaker):
+        print("Good! The code is: ", ''.join(codemaker))
+        exit()
+    else:
+        printer(records)
+
---
+++ MasterMind/solver.py
@@ -0,0 +1,25 @@
+import random
+
+# initial guess, which is a 4-repeated digit
+
+choice_0 = random.randrange(1, 5)#9)
+for _ in range(4):
+    print(choice_0, end = '')
+print()
+
+hints_specific = []
+hints_general = []
+
+def print_hints(hints) -> None:
+    for _ in hints:
+        print('\t', _)
+
+while True:
+    feedback = input('feedback: ')
+    if len(feedback) != 2:
+        continue
+    RP = feedback[0] # Right Place
+    if RP != '0':
+        hints_general.append(RP + ' of those places must be: ' + str(choice_0))
+
+    print_hints(hints_general)
