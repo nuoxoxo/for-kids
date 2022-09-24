7d2fb527ff3bb6d2d0286654163d29d0cd11ffd7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-09-25 00:02:28 +0200

    ud. solver

--- MasterMind/solver.py
+++ MasterMind/solver.py
@@ -2,7 +2,8 @@ import random
 
 # initial guess, which is a 4-repeated digit
 
-choice_0 = random.randrange(1, 5)#9)
+# choice_0 = random.randrange(1, 5)#9)
+choice_0 = 1
 for _ in range(4):
     print(choice_0, end = '')
 print()
@@ -16,10 +17,11 @@ def print_hints(hints) -> None:
 
 while True:
     feedback = input('feedback: ')
+    if feedback == 'exit':
+        exit()
     if len(feedback) != 2:
         continue
     RP = feedback[0] # Right Place
     if RP != '0':
         hints_general.append(RP + ' of those places must be: ' + str(choice_0))
-
     print_hints(hints_general)
