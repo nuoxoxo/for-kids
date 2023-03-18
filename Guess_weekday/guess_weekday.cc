df7e909d20d3c1c6499db8c1493e8474df74a85f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-18 11:42:09 +0100

    init : new impl. in ccccCcçç

--- Guess_weekday/guess_weekday.cc
+++ Guess_weekday/guess_weekday.cc
@@ -2,11 +2,15 @@
 
 int	main()
 {
-	std::time_t	result = std::time(0);
+	std::time_t	now = std::time(0);
+	std::tm		*local = std::localtime( & now );
 
-	std::cout << std::asctime(std::localtime(&result));
+	// 1st way to print time
+	std::cout << std::asctime(local);
 
+	char	buffer[32];
 
-
-
+	// 2nd way to print time
+	std::strftime(buffer, 32, "%a, %d.%m.%Y %H:%M:%S", local);
+	std::cout << buffer << std::endl;
 }
